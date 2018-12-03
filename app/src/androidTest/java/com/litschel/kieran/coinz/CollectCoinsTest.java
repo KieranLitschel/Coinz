15083ff7e524ba133c93288513cab14fe9fc901d
KieranLitschel <kieran.litschel@hotmail.co.uk>
2018-12-03 15:07:04 +0000

    Decreased time waiting for map to load after logging in in unit tests

--- app/src/androidTest/java/com/litschel/kieran/coinz/CollectCoinsTest.java
+++ app/src/androidTest/java/com/litschel/kieran/coinz/CollectCoinsTest.java
@@ -140,7 +140,7 @@ public class CollectCoinsTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(30000);
+            Thread.sleep(15000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- app/src/androidTest/java/com/litschel/kieran/coinz/MapDownloadTest.java
+++ app/src/androidTest/java/com/litschel/kieran/coinz/MapDownloadTest.java
@@ -139,7 +139,7 @@ public class MapDownloadTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(30000);
+            Thread.sleep(15000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
